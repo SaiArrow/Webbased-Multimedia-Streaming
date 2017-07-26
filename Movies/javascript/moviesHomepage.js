46b88db4ff4b4f1291c49f3a1a041d2146f3138f
Shubham-Vishwakarma <shubhamvishwakarma71@gmail.com>
2017-07-26 21:31:56 +0530

    Sliding card in 2secs time pause

--- Movies/javascript/moviesHomepage.js
+++ Movies/javascript/moviesHomepage.js
@@ -1,15 +1,2 @@
-function openNav() {
-    document.getElementById("navBar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("main").style.transition = "0.5s";
-}
+//Function for automativ slide in 2sec
 
-function closeNav() {
-    document.getElementById("navBar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("main").style.transition = "0.5s";
-}
-
-function openCloseNav(){
-	
-}
