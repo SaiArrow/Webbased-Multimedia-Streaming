a5a1b67be2935dcd6ff6c22b5d58acf3777d62d5
Shubham-Vishwakarma <shubhamvishwakarma71@gmail.com>
2017-09-16 14:36:17 +0530

    Added Pagination

--- webonlinemovies/movies/urls.py
+++ webonlinemovies/movies/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'movies'
 urlpatterns = [
 	#/movies/
-    url(r'^$',views.IndexView.as_view(),name = 'index'),
-    #/movies/215/
+    url(r'^$', views.index, name = 'index'),
     url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name = 'detail'),
+    url(r'^results/$',views.results,name = 'results'),
 ]
--- webonlinemovies/movies/views.py
+++ webonlinemovies/movies/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.template import loader
 from django.http import HttpResponse,Http404
 from django.views import generic
 from .models import Movie
-# # Create your views here.
-# def index(request):
-# 	movieList = Movie.objects.all()
-# 	return render(request,'movies/index.html',{'movieList':movieList})
 
-# def results(request,movieId):
-# 	movie = get_object_or_404(Movie,id = movieId)
-# 	return render(request,'movies/results.html',{'movie':movie})
+# # Create your views here.
+def index(request):
+	movieList = Movie.objects.all()
+	paginator = Paginator(movieList,12)
+
+	page = request.GET.get('page')
+	try:
+		movies = paginator.page(page)
+	except PageNotAnInteger:
+		# If page not an integer deliver first page
+		movies = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range deliver last page
+		movies = paginator.page(paginator.num_pages)
+	return render(request,'movies/index.html',{'movies':movies})
+
+def results(request):
+	query = request.GET.get('search')
+	resultList = Movie.objects.filter(movieName__icontains = query) | Movie.objects.filter(production__icontains = query)
+	return render(request,'movies/index.html',{'movies':resultList,'pageNo':1})
 
 class IndexView(generic.ListView):
 	template_name = 'movies/index.html'
