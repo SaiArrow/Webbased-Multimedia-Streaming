74b2bd30b1de3f69eb6e1b59419cdb698e56993b
Shubham-Vishwakarma <shubhamvishwakarma71@gmail.com>
2017-09-16 20:17:09 +0530

    Added Filter by Genre

--- webonlinemovies/movies/urls.py
+++ webonlinemovies/movies/urls.py
@@ -5,6 +5,6 @@ app_name = 'movies'
 urlpatterns = [
 	#/movies/
     url(r'^$', views.index, name = 'index'),
-    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name = 'detail'),
+    url(r'^(?P<pk>[0-9]+)/$',views.detail,name = 'detail'),
     url(r'^results/$',views.results,name = 'results'),
 ]
--- webonlinemovies/movies/views.py
+++ webonlinemovies/movies/views.py
@@ -4,10 +4,31 @@ from django.template import loader
 from django.http import HttpResponse,Http404
 from django.views import generic
 from .models import Movie
+import sys
 
 # # Create your views here.
 def index(request):
-	movieList = Movie.objects.all()
+	movieList = []
+	try:
+		choiceList = request.GET.getlist('choice')
+		print(choiceList)
+		if len(choiceList) == 0:
+			print(len(choiceList))
+			movieList = Movie.objects.all()
+		elif 'all' in choiceList:
+			print('Conatins all')
+			movieList = Movie.objects.all()
+		else:
+			print(len(choiceList))
+			for choice in choiceList:
+				try:
+					print(choice)
+					movieList = movieList | Movie.objects.filter(genre__icontains = choice)
+				except:
+					movieList = Movie.objects.filter(genre__icontains = choice)
+	except:
+		movieList = Movie.objects.all()
+	
 	paginator = Paginator(movieList,12)
 
 	page = request.GET.get('page')
@@ -26,13 +47,10 @@ def results(request):
 	resultList = Movie.objects.filter(movieName__icontains = query) | Movie.objects.filter(production__icontains = query)
 	return render(request,'movies/index.html',{'movies':resultList,'pageNo':1})
 
-class IndexView(generic.ListView):
-	template_name = 'movies/index.html'
-	context_object_name = 'movieList' #movieList is returned as object_list by default from Generic Listview class so remane it
-
-	def get_queryset(self):
-		return Movie.objects.all() 
-
 class DetailView(generic.DetailView):
 	model = Movie
 	template_name = 'movies/detail.html'
+
+def detail(request,pk):
+	movie = Movie.objects.get(pk = pk)
+	return render(request,'movies/detail.html',{'movie':movie})
