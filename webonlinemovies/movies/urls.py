984f72e0fb4fcbb6444ae00b422997e9bef28f7e
Shubham-Vishwakarma <shubhamvishwakarma71@gmail.com>
2017-09-17 22:20:51 +0530

    Added Authentication

--- webonlinemovies/movies/urls.py
+++ webonlinemovies/movies/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'movies'
 urlpatterns = [
 	#/movies/
-    url(r'^$', views.index, name = 'index'),
+    url(r'^$', views.LoginFormView.as_view(), name = 'login'),
+    url(r'^register/$',views.RegisterFormView.as_view(),name = 'register'),
+    url(r'^main/$', views.index, name = 'index'),
     url(r'^(?P<pk>[0-9]+)/$',views.detail,name = 'detail'),
     url(r'^results/$',views.results,name = 'results'),
+    url(r'^logout/$',views.logout,name="logout"),
 ]
--- webonlinemovies/movies/views.py
+++ webonlinemovies/movies/views.py
@@ -1,46 +1,49 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.template import loader
 from django.http import HttpResponse,Http404
+from django.contrib.auth import authenticate,login as django_login,logout as django_logout
 from django.views import generic
+from django .views.generic import View
+from django.conf import settings
 from .models import Movie
+from .forms import RegisterForm
 import sys
 
 # # Create your views here.
 def index(request):
-	movieList = []
-	try:
-		choiceList = request.GET.getlist('choice')
-		print(choiceList)
-		if len(choiceList) == 0:
-			print(len(choiceList))
+	if request.user.is_authenticated:
+		movieList = []
+		try:
+			choiceList = request.GET.getlist('choice')
+			print(choiceList)
+			if len(choiceList) == 0 or 'all' in choiceList:
+				print(len(choiceList))
+				movieList = Movie.objects.all()
+			else:
+				print(len(choiceList))
+				for choice in choiceList:
+					try:
+						print(choice)
+						movieList = movieList | Movie.objects.filter(genre__icontains = choice)
+					except:
+						movieList = Movie.objects.filter(genre__icontains = choice)
+		except:
 			movieList = Movie.objects.all()
-		elif 'all' in choiceList:
-			print('Conatins all')
-			movieList = Movie.objects.all()
-		else:
-			print(len(choiceList))
-			for choice in choiceList:
-				try:
-					print(choice)
-					movieList = movieList | Movie.objects.filter(genre__icontains = choice)
-				except:
-					movieList = Movie.objects.filter(genre__icontains = choice)
-	except:
-		movieList = Movie.objects.all()
 	
-	paginator = Paginator(movieList,12)
-
-	page = request.GET.get('page')
-	try:
-		movies = paginator.page(page)
-	except PageNotAnInteger:
-		# If page not an integer deliver first page
-		movies = paginator.page(1)
-	except EmptyPage:
-		# If page is out of range deliver last page
-		movies = paginator.page(paginator.num_pages)
-	return render(request,'movies/index.html',{'movies':movies})
+		paginator = Paginator(movieList,12)
+		page = request.GET.get('page')
+		try:
+			movies = paginator.page(page)
+		except PageNotAnInteger:
+			# If page not an integer deliver first page
+			movies = paginator.page(1)
+		except EmptyPage:
+			# If page is out of range deliver last page
+			movies = paginator.page(paginator.num_pages)
+		return render(request,'movies/index.html',{'movies':movies})
+	else:
+		return redirect('movies:login')
 
 def results(request):
 	query = request.GET.get('search')
@@ -54,3 +57,63 @@ class DetailView(generic.DetailView):
 def detail(request,pk):
 	movie = Movie.objects.get(pk = pk)
 	return render(request,'movies/detail.html',{'movie':movie})
+
+class LoginFormView(View):
+	template_name = 'movies/login.html'
+
+	def get(self,request):
+		return render(request,self.template_name)
+
+	def post(self,request):
+		username = request.POST['username']
+		password = request.POST['password']
+		
+		user = authenticate(username = username,password = password)
+		print(user)
+		if user is not None:
+			print('Not none',user)
+			if  user.is_active:
+				print('Active',user)
+				django_login(request,user)
+				return redirect("movies:index")
+		else:
+			print('Form is not valid')
+			return render(request,self.template_name) 			
+
+def logout(request):
+	if request.user.is_authenticated:
+		print('User is currently logged in and is requesting logout')
+		django_logout(request)
+		return redirect('movies:login')
+	else:
+		return redirect('movies:login')
+
+class RegisterFormView(View):
+	form_class = RegisterForm
+	template_name = 'movies/register.html'
+
+	def get(self,request):
+		form = self.form_class(None)
+		return render(request,self.template_name,{'form':form})
+
+	def post(self,request):
+		form = self.form_class(request.POST)
+
+		if form.is_valid():
+			user = form.save(commit = False)
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			email = form.cleaned_data['email']
+			print(username,email,password)
+			user.set_password(password)
+			user.save()
+
+			user = authenticate(username = username, password = password)
+			print(user)
+			if user is not None:
+				print('Not none',user)
+				if  user.is_active:
+					print('Active',user)
+					django_login(request,user)
+					return redirect("movies:index")
+		return render(request,self.template_name,{'form':form})
